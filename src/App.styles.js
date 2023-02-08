8f98d437bdb63d82053a5e80a73aaf4eaf2e6324
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2023-02-08 16:26:25 +0100

    feature: add Page and PageContainer styles

---
+++ src/App.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Page = styled.div`
+  height: 100vh;
+  overflow: auto;
+  background-color: var(--main-bg-color);
+  font-family: var(--main-font-family);
+`
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  min-width: 320px;
+  max-width: 1224px;
+`
