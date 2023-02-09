e54603be00889f808f5980e2b35b02b5c63db395
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2023-02-09 19:32:17 +0100

    feature: Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { LogoStyled } from 'components/Logo/Logo.styles'
+import logo from 'images/logo.svg'
+
+const Logo = () => {
+  return <LogoStyled src={logo} alt="logo" />
+}
+
+export default Logo
---
+++ src/components/Logo/Logo.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const LogoStyled = styled.img`
+  width: 94px;
+  height: 40px;
+`
