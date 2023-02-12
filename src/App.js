aea712d5ec6df881d329e9f71afb557da00f58d6
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2023-02-12 18:55:10 +0100

    feature: Table module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Page, PageContainer } from 'App.styles'
+import { Page, PageContainer, Main } from 'App.styles'
 import Header from 'modules/Header/Header'
+import Table from 'modules/Table/Table'
 
 const App = () => (
   <Page>
     <PageContainer>
       <Header />
+      <Main>
+        <Table />
+      </Main>
     </PageContainer>
   </Page>
 )
