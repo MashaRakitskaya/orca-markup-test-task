a3828ee688575ed1083d5ff523e3ee565b367bab
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2023-02-13 01:50:58 +0100

    changes: FileNameAndSize component universal reuse

--- src/components/FileNameAndSize/FileNameAndSize.styles.js
+++ src/components/FileNameAndSize/FileNameAndSize.styles.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components'
 import { secondaryColor } from 'styles/variables.styles'
 
+export const FileNameSizeContainer = styled.div`
+  display: flex;
+  gap: 4px;
+`
+
 export const FileName = styled.p`
   margin: 0px;
+  max-width: ${({ secondary }) => (secondary ? '84px' : '')};
+  white-space: nowrap;
+  overflow: ${({ secondary }) => (secondary ? 'hidden' : 'visible')};
+  text-overflow: ${({ secondary }) => (secondary ? 'ellipsis' : '')};
 `
 
 export const FileSize = styled.span`
   color: ${secondaryColor};
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
 `
